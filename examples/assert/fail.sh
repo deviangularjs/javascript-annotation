31e436f17c27f111b761b9d77bed239afb65f6ff
Dahmian Owen <Dahmian Owen@localhost>
2012-03-29 22:47:27 -0700

    renamed main

--- examples/assert/fail.sh
+++ examples/assert/fail.sh
@@ -1,2 +1,2 @@
 cd ../../build;
-java com.dahmian.javascript.annotation.Main ../examples/assert/assertSuite.js ../examples/assert/assertTestFail.js
+java com.dahmian.javascript.annotation.main ../examples/assert/assertSuite.js ../examples/assert/assertTestFail.js
